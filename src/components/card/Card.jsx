bc157578dc7049b7ea900e9605437de2e1e96474
Uulkelsin Bektemirova <100680512+ukatls@users.noreply.github.com>
2022-03-12 17:52:56 +0600

    use useState

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,10 +1,13 @@
 import css from './Card.module.css'
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className={css.wrapper}>
-      <img className={css.img} src="https://cdn.pixabay.com/photo/2021/10/07/15/23/real-estate-6688945_960_720.jpg" alt="#" />
-      <h2>$75000</h2>
+      <div className={css.imgWrapper}>
+        <img className={css.img} src={props.imgUrl} alt="#" />
+      </div>
+      <h2>{props.price}</h2>
+      <h3>{props.title}</h3>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio optio, incidunt accusamus at placeat odit nesciunt illum quod. Facere, reprehenderit!</p>
       <button>Подробнее</button>
     </div>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,52 @@
 import Card from "../components/card/Card";
+import { useState,useEffect } from "react";
 
 export default function Homepage() {
+
+  // const [numbers,setNumbers] = useState(0)
+  const [houses,setHouses] = useState([
+    {
+      id: 1,
+      title: 'Продаю дом 4к',
+      imgUrl: "https://cdn.pixabay.com/photo/2021/10/07/15/23/real-estate-6688945_960_720.jpg",
+      price: '&67000'
+    },{
+      id: 2,
+      title: 'Продаю квартиру',
+      imgUrl: "https://cdn.pixabay.com/photo/2021/10/07/15/23/real-estate-6688945_960_720.jpg",
+      price: '&6000'
+    },{
+      id: 3,
+      title: 'Продаю дом 4к',
+      imgUrl: "https://cdn.pixabay.com/photo/2021/10/07/15/23/real-estate-6688945_960_720.jpg",
+      price: '&67000'
+    }
+  ])
+
   return (
     <div className="container page">
+
+      {/* <div>
+        <button onClick={
+        ()=>{setNumbers(numbers - 1)}}>
+          Minus
+        </button>
+        <h1>{numbers}</h1>
+        <button onClick={
+        ()=>{setNumbers(numbers + 1)}}>
+          Plus
+        </button>
+      </div> */}
+
+
       <div className="title">Homepage</div>
       <div className="cardsWrapper">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {houses.map((house) => <Card
+        key={house.id}
+        title={house.title}
+        imgUrl={house.imgUrl}
+        price={house.price}
+        />)}
       </div>
     </div>
   );
