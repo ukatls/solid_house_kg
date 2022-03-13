a469c1c3fcb45bf11054bc7f4f0bc5ea7e8cb74f
Uulkelsin Bektemirova <100680512+ukatls@users.noreply.github.com>
2022-03-13 20:07:56 +0600

    card improvement

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,7 +6,7 @@ export default function Card(props) {
       <div className={css.imgWrapper}>
         <img className={css.img} src={props.imgUrl} alt="#" />
       </div>
-      <h2>{props.price}</h2>
+      <h2>$ {props.price}</h2>
       <h3>{props.title}</h3>
       <p className={css.p}>{props.description}</p>
       <button>Подробнее</button>
